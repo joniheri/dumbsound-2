cc413098c7904fc6ee2863d7e0787a9b9be5fb3f
Rizky Tanjung <kimsohyuncans@gmail.com>
2021-08-11 10:10:08 +0700

    Read data database in folder server with Rest API

--- server/src/routes/routeV1.js
+++ server/src/routes/routeV1.js
@@ -20,4 +20,15 @@ router.put("/updatetodo-put/:id", updateTodoPut);
 router.delete("/deletetodo/:id", deleteTodo);
 // EndTodosRouter
 
+// UsersRouter
+const { getUsers } = require("../controllers/User");
+
+router.get("/users", getUsers);
+// router.get("/todo/:id", getTodo);
+// router.post("/todos", addTodo);
+// router.patch("/updatetodo/:id", updateTodoPatch);
+// router.put("/updatetodo-put/:id", updateTodoPut);
+// router.delete("/deletetodo/:id", deleteTodo);
+// EndUsersRouter
+
 module.exports = router;
