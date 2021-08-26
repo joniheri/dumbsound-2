af99c30974d56578fc848b6a28dab72c1c2d53f6
joniheri <jonheri001@gmail.com>
2021-08-26 18:40:30 +0700

    Fix bug -> when isLogin true & browser closed & open the browser again, if isLogin true, we auto go to BerandaUser

--- client/src/pages/BerandaPublic.js
+++ client/src/pages/BerandaPublic.js
@@ -30,12 +30,14 @@ export default function BerandaPublic({ stateLogin, setStateLogin }) {
   const [loading, setLoading] = useState(true);
   const history = useHistory();
 
+  // console.log("DataState: ", state);
+
   // CheckUserIsLogin
-  useEffect(() => {
-    if (state.isLogin) {
-      history.push("/beranda");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (state.isLogin) {
+  //     history.push("/beranda");
+  //   }
+  // }, []);
   // EndCheckUserIsLogin
 
   // loadDatasMusic
@@ -64,151 +66,156 @@ export default function BerandaPublic({ stateLogin, setStateLogin }) {
 
   return (
     <>
-      {/* <NavbarPublic /> */}
-      <Container fluid>
-        {/* Content */}
-        <center>
-          <Row>
-            <Col lg={12} className="landingPage">
-              <p
+      {state.user == null ? (
+        <>
+          <Container fluid>
+            {/* Content */}
+            <center>
+              <Row>
+                <Col lg={12} className="landingPage">
+                  <p
+                    style={{
+                      color: "#fff",
+                      fontSize: "40px",
+                      textAlign: "center",
+                      marginTop: "190px",
+                    }}
+                  >
+                    Connect on DumbSound
+                  </p>
+                  <p
+                    style={{
+                      color: "#fff",
+                      fontSize: "20px",
+                      textAlign: "center",
+                      width: "680px",
+                    }}
+                  >
+                    Discovery, Stream, and share a constantly expanding mix of
+                    music from emerging and major artists around the world
+                  </p>
+                </Col>
+              </Row>
+            </center>
+            <div>
+              <Row
                 style={{
-                  color: "#fff",
-                  fontSize: "40px",
-                  textAlign: "center",
-                  marginTop: "190px",
+                  marginLeft: "60px",
+                  marginRight: "60px",
+                  paddingTop: "30px",
+                  marginBottom: "30px",
                 }}
               >
-                Connect on DumbSound
-              </p>
-              <p
-                style={{
-                  color: "#fff",
-                  fontSize: "20px",
-                  textAlign: "center",
-                  width: "680px",
-                }}
-              >
-                Discovery, Stream, and share a constantly expanding mix of music
-                from emerging and major artists around the world
-              </p>
-            </Col>
-          </Row>
-        </center>
-        <div>
-          <Row
-            style={{
-              marginLeft: "60px",
-              marginRight: "60px",
-              paddingTop: "30px",
-              marginBottom: "30px",
-            }}
-          >
-            <Col lg={12}>
-              <p
-                style={{
-                  textAlign: "center",
-                  color: "#EE4622",
-                  fontSize: "24px",
-                  marginBottom: "50px",
-                }}
-              >
-                Dengarkan Dan Rasakan
-              </p>
-            </Col>
-            <Col sm={12}>
-              {loading ? (
-                <center>
-                  <div className="lds-dual-ring"></div>
-                </center>
-              ) : (
-                <>
-                  <Row>
-                    {music?.map((dataMusic, index) => (
-                      <Col md={2} style={{ marginBottom: "20px" }}>
-                        <Link
-                          onClick={onSwitchLogin}
-                          style={{
-                            cursor: "pointer",
-                            textDecoration: "none",
-                          }}
-                        >
-                          <Card
-                            style={{ background: "#3A3A3A", color: "#fff" }}
-                          >
-                            <Card.Img
-                              variant="top"
-                              src={dataMusic.thumbnail}
+                <Col lg={12}>
+                  <p
+                    style={{
+                      textAlign: "center",
+                      color: "#EE4622",
+                      fontSize: "24px",
+                      marginBottom: "50px",
+                    }}
+                  >
+                    Dengarkan Dan Rasakan
+                  </p>
+                </Col>
+                <Col sm={12}>
+                  {loading ? (
+                    <center>
+                      <div className="lds-dual-ring"></div>
+                    </center>
+                  ) : (
+                    <>
+                      <Row>
+                        {music?.map((dataMusic, index) => (
+                          <Col md={2} style={{ marginBottom: "20px" }}>
+                            <Link
+                              onClick={onSwitchLogin}
                               style={{
-                                paddingTop: "6px",
-                                paddingBottom: "10px",
-                                paddingLeft: "6px",
-                                paddingRight: "6px",
+                                cursor: "pointer",
+                                textDecoration: "none",
                               }}
-                            />
-                            <Card.Body style={{ padding: "0" }}>
-                              <Card.Title
-                                style={{
-                                  fontSize: "16px",
-                                }}
+                            >
+                              <Card
+                                style={{ background: "#3A3A3A", color: "#fff" }}
                               >
-                                <Row style={{ margin: "0" }}>
-                                  <Col
-                                    md={8}
+                                <Card.Img
+                                  variant="top"
+                                  src={dataMusic.thumbnail}
+                                  style={{
+                                    paddingTop: "6px",
+                                    paddingBottom: "10px",
+                                    paddingLeft: "6px",
+                                    paddingRight: "6px",
+                                  }}
+                                />
+                                <Card.Body style={{ padding: "0" }}>
+                                  <Card.Title
                                     style={{
-                                      padding: "0",
+                                      fontSize: "16px",
+                                    }}
+                                  >
+                                    <Row style={{ margin: "0" }}>
+                                      <Col
+                                        md={8}
+                                        style={{
+                                          padding: "0",
+                                          paddingLeft: "6px",
+                                          paddingRight: "6px",
+                                        }}
+                                      >
+                                        {dataMusic.title}
+                                      </Col>
+                                      <Col
+                                        md={4}
+                                        style={{
+                                          padding: "0",
+                                          paddingLeft: "6px",
+                                          paddingRight: "6px",
+                                          textAlign: "right",
+                                        }}
+                                      >
+                                        {dataMusic.year}
+                                      </Col>
+                                    </Row>
+                                  </Card.Title>
+                                  <Card.Title
+                                    style={{
+                                      fontSize: "12px",
                                       paddingLeft: "6px",
                                       paddingRight: "6px",
                                     }}
                                   >
-                                    {dataMusic.title}
-                                  </Col>
-                                  <Col
-                                    md={4}
-                                    style={{
-                                      padding: "0",
-                                      paddingLeft: "6px",
-                                      paddingRight: "6px",
-                                      textAlign: "right",
-                                    }}
-                                  >
-                                    {dataMusic.year}
-                                  </Col>
-                                </Row>
-                              </Card.Title>
-                              <Card.Title
-                                style={{
-                                  fontSize: "12px",
-                                  paddingLeft: "6px",
-                                  paddingRight: "6px",
-                                }}
-                              >
-                                {dataMusic.artist.name}
-                              </Card.Title>
-                            </Card.Body>
-                          </Card>
-                        </Link>
-                      </Col>
-                    ))}
-                  </Row>
-                </>
-              )}
-            </Col>
-          </Row>
-          <ModalLogin
-            loginShow={loginShow}
-            setLoginShow={setLoginShow}
-            setRegisterShow={setRegisterShow}
-            stateLogin={stateLogin}
-            setStateLogin={setStateLogin}
-          />
-          <ModalRegister
-            registerShow={registerShow}
-            setRegisterShow={setRegisterShow}
-            setLoginShow={setLoginShow}
-          />
-        </div>
-        {/* End Content */}
-      </Container>
+                                    {dataMusic.artist.name}
+                                  </Card.Title>
+                                </Card.Body>
+                              </Card>
+                            </Link>
+                          </Col>
+                        ))}
+                      </Row>
+                    </>
+                  )}
+                </Col>
+              </Row>
+              <ModalLogin
+                loginShow={loginShow}
+                setLoginShow={setLoginShow}
+                setRegisterShow={setRegisterShow}
+                stateLogin={stateLogin}
+                setStateLogin={setStateLogin}
+              />
+              <ModalRegister
+                registerShow={registerShow}
+                setRegisterShow={setRegisterShow}
+                setLoginShow={setLoginShow}
+              />
+            </div>
+            {/* End Content */}
+          </Container>
+        </>
+      ) : (
+        <>{history.push("/beranda")}</>
+      )}
     </>
   );
 }
